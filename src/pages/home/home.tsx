62786b3b2c1b79d4ab2e00e7151a01d68eafb815
André Miranda <aegis.andre@gmail.com>
2025-01-23 10:25:26 -0300

    :sparkles: feat(home): add revenue section

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,33 @@
+import { useHouseholdBudget } from "@/store/use-household-budget"
+import { BudgetSection } from "./budget-section/budget-section"
+import { CurrencyInput } from "@/components/ui/currency-input/currency-input"
+import { formatCurrency } from "@/utils/format-currency"
+
 export function Home() {
+  const { setRevenue, salary, otherRevenues, revenueTotal } =
+    useHouseholdBudget()
+
   return (
     <>
-      <h1>Hello world!</h1>
+      <header className="container">
+        <h1>Gastos Domésticos</h1>
+      </header>
+
+      <main className="container">
+        <BudgetSection title="Receita" total={formatCurrency(revenueTotal())}>
+          <CurrencyInput
+            label="salario"
+            value={salary}
+            setValue={(amount) => setRevenue("salary", amount)}
+          />
+
+          <CurrencyInput
+            label="outros rendimentos"
+            value={otherRevenues}
+            setValue={(amount) => setRevenue("otherRevenues", amount)}
+          />
+        </BudgetSection>
+      </main>
     </>
   )
 }
