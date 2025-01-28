d815f7a8e0bebff0b4cc6cb8265d5013f6734305
André Miranda <aegis.andre@gmail.com>
2025-01-28 10:03:48 -0300

    :package: build(dependencies): add sonner, toast library

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import { Toaster } from "sonner"
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Toaster expand richColors position="top-center" />
       <Outlet />
       <TanStackRouterDevtools />
     </>
